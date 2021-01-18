bb4384e8d9f20e33d153d6df9359a26f902836be
JuniorLopes <juniorlopes@MacBook-Pro-de-JuniorLopes.local>
2021-01-17 23:30:57 -0300

    home body init

--- lib/app/modules/home/components/home_body.dart
+++ lib/app/modules/home/components/home_body.dart
@@ -3,6 +3,61 @@ import 'package:flutter/material.dart';
 class HomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        GridView.count(
+          crossAxisCount: 3,
+          primary: false,
+          scrollDirection: Axis.vertical,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          padding: const EdgeInsets.all(10),
+          children: [
+            Card(
+              child: InkWell(
+                onTap: () {},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.monetization_on,
+                      size: 28,
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      'Novo Sorteio',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              child: InkWell(
+                onTap: () {},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.receipt,
+                      size: 28,
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      'Histórico',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
