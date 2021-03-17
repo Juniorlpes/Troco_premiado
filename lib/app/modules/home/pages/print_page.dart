02eb845ee74e4c2426710439f70e01a360e58c14
JuniorLopes <juniorlopes@MacBook-Pro-de-JuniorLopes.local>
2021-03-17 10:55:23 -0300

    coment print pagr

--- lib/app/modules/home/pages/print_page.dart
+++ lib/app/modules/home/pages/print_page.dart
@@ -1,9 +1,4 @@
-import 'dart:convert';
-
-import 'package:bluetooth_print/bluetooth_print.dart';
-import 'package:bluetooth_print/bluetooth_print_model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:troco_premiado/shared/models/ticket_raffle.dart';
 
 class PrintPage extends StatefulWidget {
@@ -16,52 +11,52 @@ class PrintPage extends StatefulWidget {
 }
 
 class _PrintPageState extends State<PrintPage> {
-  BluetoothPrint bluetoothPrint = BluetoothPrint.instance;
+  // BluetoothPrint bluetoothPrint = BluetoothPrint.instance;
 
-  bool _connected = false;
-  BluetoothDevice _device;
-  String tips = 'no device connect';
+  // bool _connected = false;
+  // BluetoothDevice _device;
+  // String tips = 'no device connect';
 
-  @override
-  void initState() {
-    super.initState();
-    initBluetooth();
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   initBluetooth();
+  // }
 
-  Future<void> initBluetooth() async {
-    bluetoothPrint.startScan(timeout: Duration(seconds: 4));
+  // Future<void> initBluetooth() async {
+  //   bluetoothPrint.startScan(timeout: Duration(seconds: 4));
 
-    bool isConnected = await bluetoothPrint.isConnected;
+  //   bool isConnected = await bluetoothPrint.isConnected;
 
-    bluetoothPrint.state.listen((state) {
-      print('cur device status: $state');
+  //   bluetoothPrint.state.listen((state) {
+  //     print('cur device status: $state');
 
-      switch (state) {
-        case BluetoothPrint.CONNECTED:
-          setState(() {
-            _connected = true;
-            tips = 'connect success';
-          });
-          break;
-        case BluetoothPrint.DISCONNECTED:
-          setState(() {
-            _connected = false;
-            tips = 'disconnect success';
-          });
-          break;
-        default:
-          break;
-      }
-    });
+  //     switch (state) {
+  //       case BluetoothPrint.CONNECTED:
+  //         setState(() {
+  //           _connected = true;
+  //           tips = 'connect success';
+  //         });
+  //         break;
+  //       case BluetoothPrint.DISCONNECTED:
+  //         setState(() {
+  //           _connected = false;
+  //           tips = 'disconnect success';
+  //         });
+  //         break;
+  //       default:
+  //         break;
+  //     }
+  //   });
 
-    if (!mounted) return;
+  //   if (!mounted) return;
 
-    if (isConnected) {
-      setState(() {
-        _connected = true;
-      });
-    }
-  }
+  //   if (isConnected) {
+  //     setState(() {
+  //       _connected = true;
+  //     });
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -70,146 +65,146 @@ class _PrintPageState extends State<PrintPage> {
         title: Text('Imprimir biblhete'),
         centerTitle: true,
       ),
-      floatingActionButton: StreamBuilder<bool>(
-        stream: bluetoothPrint.isScanning,
-        initialData: false,
-        builder: (c, snapshot) {
-          if (snapshot.data) {
-            return FloatingActionButton(
-              child: Icon(Icons.stop),
-              onPressed: () => bluetoothPrint.stopScan(),
-              backgroundColor: Colors.red,
-            );
-          } else {
-            return FloatingActionButton(
-                child: Icon(Icons.search),
-                onPressed: () =>
-                    bluetoothPrint.startScan(timeout: Duration(seconds: 4)));
-          }
-        },
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                  child: Text(tips),
-                ),
-              ],
-            ),
-            Divider(),
-            StreamBuilder<List<BluetoothDevice>>(
-              stream: bluetoothPrint.scanResults,
-              initialData: [],
-              builder: (c, snapshot) => Column(
-                children: snapshot.data
-                    .map((d) => ListTile(
-                          title: Text(d.name ?? ''),
-                          subtitle: Text(d.address),
-                          onTap: () async {
-                            setState(() {
-                              _device = d;
-                            });
-                          },
-                          trailing:
-                              _device != null && _device.address == d.address
-                                  ? Icon(
-                                      Icons.check,
-                                      color: Colors.green,
-                                    )
-                                  : null,
-                        ))
-                    .toList(),
-              ),
-            ),
-            Divider(),
-            Container(
-              padding: EdgeInsets.fromLTRB(20, 5, 20, 10),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      OutlineButton(
-                        child: Text('connect'),
-                        onPressed: _connected
-                            ? null
-                            : () async {
-                                if (_device != null &&
-                                    _device.address != null) {
-                                  await bluetoothPrint.connect(_device);
-                                } else {
-                                  setState(() {
-                                    tips = 'please select device';
-                                  });
-                                  print('please select device');
-                                }
-                              },
-                      ),
-                      SizedBox(width: 10.0),
-                      OutlineButton(
-                        child: Text('disconnect'),
-                        onPressed: _connected
-                            ? () async {
-                                await bluetoothPrint.disconnect();
-                              }
-                            : null,
-                      ),
-                    ],
-                  ),
-                  OutlineButton(
-                    child: Text('imprimir bilhete'),
-                    onPressed: _connected
-                        ? () async {
-                            Map<String, dynamic> config = Map();
-                            List<LineText> list = List();
-                            list.add(LineText(
-                                type: LineText.TYPE_TEXT,
-                                content: 'A Title',
-                                weight: 1,
-                                align: LineText.ALIGN_CENTER,
-                                linefeed: 1));
-                            list.add(LineText(
-                                type: LineText.TYPE_TEXT,
-                                content: 'this is conent left',
-                                weight: 0,
-                                align: LineText.ALIGN_LEFT,
-                                linefeed: 1));
-                            list.add(LineText(
-                                type: LineText.TYPE_TEXT,
-                                content: 'this is conent right',
-                                align: LineText.ALIGN_RIGHT,
-                                linefeed: 1));
-                            list.add(LineText(linefeed: 1));
-//                            list.add(LineText(type: LineText.TYPE_BARCODE, content: 'A12312112', size:10, align: LineText.ALIGN_CENTER, linefeed: 1));
-//                            list.add(LineText(linefeed: 1));
-//                            list.add(LineText(type: LineText.TYPE_QRCODE, content: 'qrcode i', size:10, align: LineText.ALIGN_CENTER, linefeed: 1));
-//                            list.add(LineText(linefeed: 1));
+//       floatingActionButton: StreamBuilder<bool>(
+//         stream: bluetoothPrint.isScanning,
+//         initialData: false,
+//         builder: (c, snapshot) {
+//           if (snapshot.data) {
+//             return FloatingActionButton(
+//               child: Icon(Icons.stop),
+//               onPressed: () => bluetoothPrint.stopScan(),
+//               backgroundColor: Colors.red,
+//             );
+//           } else {
+//             return FloatingActionButton(
+//                 child: Icon(Icons.search),
+//                 onPressed: () =>
+//                     bluetoothPrint.startScan(timeout: Duration(seconds: 4)));
+//           }
+//         },
+//       ),
+//       body: SingleChildScrollView(
+//         child: Column(
+//           children: <Widget>[
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: <Widget>[
+//                 Padding(
+//                   padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+//                   child: Text(tips),
+//                 ),
+//               ],
+//             ),
+//             Divider(),
+//             StreamBuilder<List<BluetoothDevice>>(
+//               stream: bluetoothPrint.scanResults,
+//               initialData: [],
+//               builder: (c, snapshot) => Column(
+//                 children: snapshot.data
+//                     .map((d) => ListTile(
+//                           title: Text(d.name ?? ''),
+//                           subtitle: Text(d.address),
+//                           onTap: () async {
+//                             setState(() {
+//                               _device = d;
+//                             });
+//                           },
+//                           trailing:
+//                               _device != null && _device.address == d.address
+//                                   ? Icon(
+//                                       Icons.check,
+//                                       color: Colors.green,
+//                                     )
+//                                   : null,
+//                         ))
+//                     .toList(),
+//               ),
+//             ),
+//             Divider(),
+//             Container(
+//               padding: EdgeInsets.fromLTRB(20, 5, 20, 10),
+//               child: Column(
+//                 children: <Widget>[
+//                   Row(
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     children: <Widget>[
+//                       OutlineButton(
+//                         child: Text('connect'),
+//                         onPressed: _connected
+//                             ? null
+//                             : () async {
+//                                 if (_device != null &&
+//                                     _device.address != null) {
+//                                   await bluetoothPrint.connect(_device);
+//                                 } else {
+//                                   setState(() {
+//                                     tips = 'please select device';
+//                                   });
+//                                   print('please select device');
+//                                 }
+//                               },
+//                       ),
+//                       SizedBox(width: 10.0),
+//                       OutlineButton(
+//                         child: Text('disconnect'),
+//                         onPressed: _connected
+//                             ? () async {
+//                                 await bluetoothPrint.disconnect();
+//                               }
+//                             : null,
+//                       ),
+//                     ],
+//                   ),
+//                   OutlineButton(
+//                     child: Text('imprimir bilhete'),
+//                     onPressed: _connected
+//                         ? () async {
+//                             Map<String, dynamic> config = Map();
+//                             List<LineText> list = List();
+//                             list.add(LineText(
+//                                 type: LineText.TYPE_TEXT,
+//                                 content: 'A Title',
+//                                 weight: 1,
+//                                 align: LineText.ALIGN_CENTER,
+//                                 linefeed: 1));
+//                             list.add(LineText(
+//                                 type: LineText.TYPE_TEXT,
+//                                 content: 'this is conent left',
+//                                 weight: 0,
+//                                 align: LineText.ALIGN_LEFT,
+//                                 linefeed: 1));
+//                             list.add(LineText(
+//                                 type: LineText.TYPE_TEXT,
+//                                 content: 'this is conent right',
+//                                 align: LineText.ALIGN_RIGHT,
+//                                 linefeed: 1));
+//                             list.add(LineText(linefeed: 1));
+// //                            list.add(LineText(type: LineText.TYPE_BARCODE, content: 'A12312112', size:10, align: LineText.ALIGN_CENTER, linefeed: 1));
+// //                            list.add(LineText(linefeed: 1));
+// //                            list.add(LineText(type: LineText.TYPE_QRCODE, content: 'qrcode i', size:10, align: LineText.ALIGN_CENTER, linefeed: 1));
+// //                            list.add(LineText(linefeed: 1));
 
-                            ByteData data = await rootBundle
-                                .load("assets/images/bluetooth_print.png");
-                            List<int> imageBytes = data.buffer.asUint8List(
-                                data.offsetInBytes, data.lengthInBytes);
-                            String base64Image = base64Encode(imageBytes);
-                            list.add(LineText(
-                                type: LineText.TYPE_IMAGE,
-                                content: base64Image,
-                                align: LineText.ALIGN_CENTER,
-                                linefeed: 1));
+//                             ByteData data = await rootBundle
+//                                 .load("assets/images/bluetooth_print.png");
+//                             List<int> imageBytes = data.buffer.asUint8List(
+//                                 data.offsetInBytes, data.lengthInBytes);
+//                             String base64Image = base64Encode(imageBytes);
+//                             list.add(LineText(
+//                                 type: LineText.TYPE_IMAGE,
+//                                 content: base64Image,
+//                                 align: LineText.ALIGN_CENTER,
+//                                 linefeed: 1));
 
-                            await bluetoothPrint.printReceipt(config, list);
-                          }
-                        : null,
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
+//                             await bluetoothPrint.printReceipt(config, list);
+//                           }
+//                         : null,
+//                   ),
+//                 ],
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
     );
   }
 }
